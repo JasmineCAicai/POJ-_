4d56d89cf11cece8b95d270173fc73b8f4737dde
JasmineCAicai <48902911+JasmineCAicai@users.noreply.github.com>
2021-03-12 09:36:52 +0800

    Create poj-2734.cpp

---
+++ 数制转换问题/poj-2734.cpp
@@ -0,0 +1,20 @@
+// 十进制转八进制
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int i, j, q, r = 0;
+	int t[100] = { 0 };
+	cin >> q;
+	for (i = 0; q != 0; i++) {
+		t[i] = q % 8;
+		q = q / 8;
+	}
+	for (j = i - 1; j >= 0; j--) {
+		cout << t[j];
+	}
+	cout << endl;
+
+	return 0;
+}
